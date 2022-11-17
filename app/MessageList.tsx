eb6a7d83e2afe914b078c5cfac23e90c237ca478
Tha_UI_Guy <adeniran1059@gmail.com>
2022-11-18 00:31:24 +0100

    Setup Redis and Layouts

---
+++ app/MessageList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MessageList() {
+  return (
+    <div>
+      <p>Messeg here</p>
+      <p>Messeg here</p>
+      <p>Messeg here</p>
+      <p>Messeg here</p>
+      <p>Messeg here</p>
+      <p>Messeg here</p>
+    </div>
+  );
+}
+
+export default MessageList;
